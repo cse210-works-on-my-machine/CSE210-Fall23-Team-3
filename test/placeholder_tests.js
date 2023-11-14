a58e99df1dcefc46d762542fd5c0687255b4f3d2
Varun Singh <dualsaturn@gmail.com>
2023-11-14 08:29:36 -0800

    added basic tests

---
+++ test/placeholder_tests.js
@@ -0,0 +1,10 @@
+const test = require('node:test');
+const { describe, it } = require('node:test');
+const assert = require('assert')
+
+// Placeholder nodejs native tests
+describe('Placeholder Tests', () => {
+    it('should pass', () => {
+        assert.strictEqual(1, 1);
+    });
+});
