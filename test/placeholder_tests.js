be3e4e2098170c19e10e9b05d57c003148b932f7
crossboy <itscrossboy@gmail.com>
2023-11-25 14:11:59 -0800

    Add auto fix on save and basic configuration for style

--- test/placeholder_tests.js
+++ test/placeholder_tests.js
@@ -1,10 +1,10 @@
-const test = require('node:test');
-const { describe, it } = require('node:test');
-const assert = require('assert')
+const test = require("node:test");
+const { describe, it } = require("node:test");
+const assert = require("assert");
 
 // Placeholder nodejs native tests
-describe('Placeholder Tests', () => {
-    it('should pass', () => {
+describe("Placeholder Tests", () => {
+    it("should pass", () => {
         assert.strictEqual(1, 1);
     });
 });
