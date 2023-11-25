63dba788275ac2e417b8393f5a8aa5a445380b90
Om Prakaash <omsuseela@gmail.com>
2023-11-25 11:55:34 -0800

    More formatting. Display huge author avatars

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
+import {Post} from './src/post.js';
+
 document.addEventListener('DOMContentLoaded', function() {
     displayPostsNew();
 });
-
-
-function fetchFeaturedTags() {
-    const apiUrl = 'https://mastodon.social/api/v1/trends/tags';
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(tags => tags.forEach(tag => fetchPostsByHashtag(tag.name)))
-        .catch(error => console.error('Error fetching featured tags:', error));
-}
 
 /**
  * 
@@ -36,7 +28,7 @@ async function fetchTrendingTags() {
  * @returns 
  */
 async function fetchPostsByHashtag(hashtag) {
-    const endpoints = `https://mastodon.social/api/v1/timelines/tag/:${hashtag}`;
+    const endpoint = `https://mastodon.social/api/v1/timelines/tag/:${hashtag}`;
     try {
         const response = await fetch(endpoint);
         if (!response.ok) {
@@ -60,33 +52,10 @@ async function displayPostsNew() {
         section.innerHTML = `<h2>#${tag.name}</h2>`;
         const posts = await fetchPostsByHashtag(tag.name);
         posts.forEach(post => {
-            const postDiv = document.createElement('div');
-            postDiv.innerHTML = `<p>${post.content}</p>`; // Sanitize this content
+            const p = Post.fromJSON(post);
+            const postDiv = p.getDisplayDiv();
             section.appendChild(postDiv);
         });
         container.appendChild(section);
     });
-}
-
-
-
-function fetchPostsByHashtag(hashtag) {
-    const apiUrl = `https://mastodon.social/api/v1/timelines/tag/:${hashtag}`;
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => displayPosts(hashtag, data))
-        .catch(error => console.error(`Error fetching posts for #${hashtag}:`, error));
-}
-
-function displayPosts(hashtag, data) {
-    const container = document.getElementById('featuredTagsPosts');
-    const section = document.createElement('section');
-    section.innerHTML = `<h2>#${hashtag}</h2>`;
-    data.forEach(post => {
-        const postDiv = document.createElement('div');
-        postDiv.innerHTML = `<p>${post.content}</p>`; // Sanitize this content
-        section.appendChild(postDiv);
-    });
-    container.appendChild(section);
 }
---
+++ src/post.js
@@ -0,0 +1,42 @@
+ 
+
+export class Post {
+    
+    constructor(id, content, authorName, createdAt, authorImage) {
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName; // Assuming 'author' is a simple string or object
+        this.createdAt = createdAt;
+        this.authorImageURL = authorImage
+    }
+
+    static fromJSON(json) {
+        return new Post(json.id, json.content, json.account.username, json.createdAt, json.account.avatar);
+    }
+
+    getDisplayDiv() {
+
+        const postDiv = document.createElement('div');
+        postDiv.className = 'post';
+
+        // Author image
+        if (this.authorImageURL) {
+            console.log("Image found");
+            const image = document.createElement('img');
+            image.src = this.authorImageURL;
+            image.alt = `Image of ${this.authorName}`;
+            image.className = 'author-image';
+            postDiv.appendChild(image);
+        }
+        else{
+            console.log("No image found");
+        }
+
+        // Post content
+        const contentParagraph = document.createElement('p');
+        contentParagraph.innerHTML = this.content; 
+        postDiv.appendChild(contentParagraph);
+
+        return postDiv;
+    }
+}
