7dc953bf7dc69a2a63a4d91d139a213a5ffe459b
Jackson Conte <jacksonjconte@gmail.com>
2023-12-05 01:01:06 -0800

    some untested progress

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -1,29 +1,69 @@
 const DEFAULT_LISTS = {
-    mastodon:["https://mastodon.social"],
-    lemmy:["https://lemmy.ml"],
-}
-// TODO: figure out how to generalize this to any Fediverse network
-// Could use dependency injection and pass the network name
-// and then look up local storage instance list
-function addInstance(network) {
-
+    mastodon:['https://mastodon.social', 'https://fosstodon.org'],
+    lemmy:['https://lemmy.ml']
 }
 
-function removeInstance(network) {
+const INST_LISTS = 'instanceLists';
 
+/**
+ * Wrapper function for local storage object conversion.
+ * @precondition obj_str must either be a valid JSON string or null.
+ * This shouldn't be a problem unless local storage is corrupted.
+ * @param {string | null} obj_str 
+ * @returns {object} JSON parsed object string, or an empty object if null
+ */
+function to_obj(obj_str) {
+    return (obj_str) === null? {} : JSON.parse(obj_str)
 }
 
 /**
- * @param network A string indicating the name of the network this is an instance of 
- * @param url The URL to ping as a valid network instance
- * @returns True if the instance URL is valid, False otherwise
+ * @param {string} network The name of the network (e.g. 'mastodon' or 'lemmy')
+ * @param {*} url The URL of the instance to add.
  */
-function verifyInstance(network, url) {
-    return true;
+function addInstance(network, url) {
+    // TODO: move call out of this function? (may separate concerns better)
+    if (!validInstance(network, url)) {
+        return;
+    }
+    let instanceList = to_obj(localStorage.getItem(INST_LISTS));
+    if (network in instanceList) {
+        instanceList[network].push(url);
+    } else {
+        instanceList[network] = [...DEFAULT_LISTS[network]].append(url);
+    }
+    localStorage.setItem(INST_LISTS, JSON.stringify(instanceList));
 }
 
-function resetInstanceList() {
+/**
+ * Removes an instance from localStorage. Currently O(n) with respect to number of instances.
+ * @param {*} network 
+ * @param {*} url 
+ */
+function removeInstance(network, url) {
 
+}
+
+// TODO: how tf is this async gonna work
+/**
+ * @param {string} network A string indicating the name of the network this is an instance of 
+ * @param {string} url The URL to ping as a valid network instance
+ * @returns {boolean} True if the instance URL is valid, False otherwise
+ */
+async function validInstance(network, url) {
+    // TODO: refactor from branching logic
+    if (network === 'mastodon') {
+        // TODO: look into using instance info
+        const response = await fetch(`${url}/api/v2/instance`);
+        return response.status === 200;
+    }
+}
+
+/**
+ * 
+ * @param {string} network 
+ */
+function resetInstanceList(network) {
+    
 }
 
 // useful for fetching stuff/algorithm, read from local storage
@@ -32,11 +72,14 @@ function resetInstanceList() {
 /**
  * 
  * @param {string} network The name of the Fediverse network.
- * @returns A list of instance URLs the user has configured, or a default list if none are available.
+ * @returns {Array} A list of instance URLs the user has configured, or a default list if none are available,
+ * or an empty list if the network name is invalid (should never happen due to verifyInstance)
  */
+
+// TODO: design decision -- fetch localStorage every time?
 function getInstanceList(network) {
-    // TODO: replace instanceLists with local storage query
-    let instanceLists = {}
+    let instanceLists = to_obj(localStorage.getItem(INST_LISTS));
+
     if (network in instanceLists) {
         return instanceLists[network];
     } else if (network in DEFAULT_LISTS) {
