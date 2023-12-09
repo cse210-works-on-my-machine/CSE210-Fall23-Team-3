b76e1752dba36ea3f3677f98f8c47ffe64af752c
Om Prakaash <omsuseela@gmail.com>
2023-12-09 15:01:32 -0800

    Add test for paginator

---
+++ test/paginator_test.js
@@ -0,0 +1,77 @@
+import {describe, it, after, afterEach, before, beforeEach} from 'node:test';
+import {JSDOM} from 'jsdom';
+import * as assert from 'assert';
+
+describe('Paginator', () => {
+    let paginatorImport;
+    let paginator;
+    const mockPosts = [
+        {
+          id: '1',
+          content: 'This is the first post',
+          authorName: 'John Doe',
+          createdAt: '2023-12-09',
+          authorImageURL: 'https://example.com/avatar1.jpg',
+          authorHandle: '@johndoe',
+        },
+        {
+          id: '2',
+          content: 'This is the second post',
+          authorName: 'Jane Doe',
+          createdAt: '2023-12-09',
+          authorImageURL: 'https://example.com/avatar2.jpg',
+          authorHandle: '@janedoe',
+        },
+        {
+          id: '3',
+          content: 'This is the third post',
+          authorName: 'Alex Smith',
+          createdAt: '2023-12-09',
+          authorImageURL: 'https://example.com/avatar3.jpg',
+          authorHandle: '@alexsmith',
+        },
+    ];
+
+    let dom;
+    let container;
+    let pageLabel;
+
+    beforeEach(() => {
+        dom = new JSDOM('<!doctype html><html><body><div id="featuredTagsPosts"></div><span id="page-label"></span></body></html>', {
+            url: 'http://localhost'
+        });
+        global.document = dom.window.document;
+        global.window = dom.window;
+        global.navigator = dom.window.navigator;
+        global.HTMLElement = dom.window.HTMLElement;
+        global.customElements = dom.window.customElements;
+
+        container = document.getElementById('featuredTagsPosts');
+        pageLabel = document.getElementById('page-label');
+    });
+
+    describe('#displayPage1AtStart', () => {
+      it('should display first 2 posts (page 1) at start', async function() {
+
+        const posts = mockPosts.map(postData => {
+            const postElement =  global.document.createElement('fedi-post');
+            postElement.setAttribute('id', postData.id);
+            postElement.setAttribute('content', postData.content);
+            postElement.setAttribute('author-name', postData.authorName);
+            postElement.setAttribute('created-at', postData.createdAt);
+            postElement.setAttribute('author-image-url', postData.authorImageURL);
+            postElement.setAttribute('author-handle', postData.authorHandle);
+            return postElement;
+            });
+
+        // Import the paginator
+        paginatorImport = await import('../src/scripts/paginator.js');
+        paginator = new paginatorImport.Paginator(posts, 2);
+
+        // Should display the first two posts
+        let featuredTagsPosts = document.getElementById('featuredTagsPosts');
+        assert.strictEqual(featuredTagsPosts.children.length, 2);
+
+      });
+    });
+});
