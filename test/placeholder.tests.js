6533d7ad1457eca237ad677235c3190a5b52eb37
crossboy <itscrossboy@gmail.com>
2023-12-12 06:04:57 -0800

    Remove placeholder test

--- test/placeholder.tests.js
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'node:test';
-import * as assert from 'assert';
-
-// Placeholder nodejs native tests
-describe("Placeholder Tests", () => {
-    it("should pass", () => {
-        assert.strictEqual(1, 1);
-    });
-});
